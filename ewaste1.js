c275abea409106f80bb7c9a157f785a8c1c1605c
jamierafaela <hrppagkalinawan27@gmail.com>
2025-03-31 01:37:42 +0800

    Delete ewaste1.js

--- ewaste1.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) { // Adjust scroll threshold
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
